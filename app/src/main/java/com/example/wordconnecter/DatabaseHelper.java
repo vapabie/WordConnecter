0a336b09d77ed02e366c8938a04a3c2fbd361793
Papp Gréta <papp.greta2@gmail.com>
2023-09-22 18:19:41 +0200

    Added DatabaseHelper and DatabaseManager classes.

---
+++ app/src/main/java/com/example/wordconnecter/DatabaseHelper.java
@@ -0,0 +1,34 @@
+package com.example.wordconnecter;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME="WordConnector.db";//adatbázis neve
+    private static final int DATABASE_VERSION=1;//adatbázis verziója
+    private static final String TABLA_WORDS="words";//adatbázis TÁBLA neve
+    private static final String COLUMN_ID="id";//Az id/azonosító oszlop
+    private static final String COLUMN_HUN="hun";//magyar szavak oszlopa
+    private static final String COLUMN_EN="en";//angol szavak oszlopa
+
+
+    private static final String TABLE_CREATE =
+            "CREATE TABLE " + TABLA_WORDS + " (" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + COLUMN_HUN + "TEXT, " + COLUMN_EN + " TEXT);";//Ezzel az SQL paranccsal hozza létre a TÁBLÁT
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);//konstruktor inicializálásra
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(TABLE_CREATE);//Ezzel az SQL paranccsal hozza létre az ADATBÁZIST
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLA_WORDS);//ha létezne már egy tábla, azt eldobja
+        onCreate(db);//meghívja az onCreate-et és létrehozza a táblát
+    }
+}
