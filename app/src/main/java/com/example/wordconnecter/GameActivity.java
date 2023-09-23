45e630fc252e22a08034f69964d6d833be05ad33
Varga Petra <vargapetra0123@gmail.com>
2023-09-23 14:45:52 +0200

    Add GameActivity.java

---
+++ app/src/main/java/com/example/wordconnecter/GameActivity.java
@@ -0,0 +1,15 @@
+package com.example.wordconnecter;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_screen);
+    }
+
+}
