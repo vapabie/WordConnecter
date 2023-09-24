c566a19634a9cc2d4fe20d71a90cf34312eed2ac
Varga Petra <vargapetra0123@gmail.com>
2023-09-24 12:15:33 +0200

    Display username

--- app/src/main/java/com/example/wordconnecter/ScoreActivity.java
+++ app/src/main/java/com/example/wordconnecter/ScoreActivity.java
@@ -3,15 +3,24 @@ package com.example.wordconnecter;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ScoreActivity extends AppCompatActivity {
 
+    private TextView scoretextTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.score_screen);
+
+        scoretextTextView = findViewById(R.id.scoretext);
+
+        String username =getIntent().getStringExtra("username");
+
+        scoretextTextView.setText(username + ", your\nscore is:");
     }
 
     public void startGameActivity (View view){
